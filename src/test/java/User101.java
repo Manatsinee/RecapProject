dda0ef035df5f9038f5fe2859d2d49d13b6f4bbe
Manatsinee <littlebring@gmail.com>
2021-06-30 20:25:56 -0400

    User Story 101 has been completed

--- src/test/java/User101.java
+++ src/test/java/User101.java
@@ -1,5 +1,7 @@
 public class User101 {
     public static void main(String[] args) {
+        System.out.println("Awesome! work has been done!!");
+        System.out.println("So i can get my code reviewed and merged into the master in the codebase");
 
     }
 }
